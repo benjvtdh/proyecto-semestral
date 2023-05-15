cc0f1ebd35e43b120238dc8bf13c373d0eb7bb6e
benjv_tdh <cortesbenja@gmail.com>
2023-05-14 23:38:20 -0400

    agregando cambios a api

--- js/api.js
+++ js/api.js
@@ -7,19 +7,12 @@ function latitudLongitud(posicion) {
   const tiempo = document.querySelector(".tiempo");
 
   fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitud}&longitude=${longitud}&localityLanguage=es`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const comuna = data.locality;
-      comunaHtml.innerHTML += comuna;
-    });
-
-  fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${appiKey}&units=${unidadMedida}`
   )
     .then((response) => response.json())
     .then((data) => {
+      const comuna = data.name;
+      comunaHtml.innerHTML += comuna;
       const iconoTiempo = document.createElement("img");
       console.log(tiempo);
       const iconoTiempoUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
